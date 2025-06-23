de462a4068d8872b60943149c971582cfcaa162a
athul457 <athults457@gmail.com>
2025-06-23 12:41:08 +0530

    functionality completed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,18 @@
 export function Footer({ itemArray }) {
   const itemLength = itemArray.length;
-  const completedItem = itemArray.completed;
+  const completedItem = itemArray.filter((item) => item.completed).length;
+  const completePercentage =
+    itemLength === 0 ? 0 : Math.round((completedItem / itemLength) * 100);
   return (
     <>
-      <div className="absolute bottom-20 left-165">
-        <h1 className="font-bold text-xl">
-          you have {itemLength} items on your list and {completedItem}
-        </h1>
+      <div className="absolute bottom-20 left-135">
+        <div className="font-bold text-xl text-red-500">
+          <span className="italic text-black">you have</span> {itemLength}{" "}
+          <span className="italic text-black">items on your list and</span>{" "}
+          {completedItem}{" "}
+          <span className="mr-2 italic text-black">completed</span>
+          {completePercentage} %
+        </div>
       </div>
     </>
   );
